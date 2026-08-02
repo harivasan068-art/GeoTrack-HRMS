ec2c36d09f16c6c355dca42ab4a03c0317fd9f13
Hari Vasan <harivasan@gmail.com>
2026-08-03 02:37:07 +0530

    fix(alembic): repair migration chain down_revision to point to 001_initial_migration

--- backend/alembic/versions/004_add_work_proofs_table.py
+++ backend/alembic/versions/004_add_work_proofs_table.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '004_add_work_proofs'
-down_revision: Union[str, None] = '003_add_selfie_url'
+down_revision: Union[str, None] = '001_initial_migration'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
